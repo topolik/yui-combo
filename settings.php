ca9c699653a056b8ea73c4f09636e0fc09f333c2
Tomáš Polešovský <tomas.polesovsky@liferay.com>
2022-10-20 20:19:00 +0200

    Change defaults to an existing dir

--- settings.php
+++ settings.php
@@ -1,7 +1,7 @@
 <?php
 define(COMBO_FILE_PATH, dirname(__FILE__));
 
-define(YUI_BUILD_PATH, COMBO_FILE_PATH . '/../../build');
+define(YUI_BUILD_PATH, COMBO_FILE_PATH . '/..');
 
 define(TEMP_DIR, '/tmp/yuicombo');
 
